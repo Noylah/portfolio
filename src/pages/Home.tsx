9d04c4840415013861dc3e0837dccc412a1c61c5
Noylah <pucci8047@gmail.com>
2026-05-30 21:11:23 +0200

    Margin Home (mobile) sistemato, titoli dinamici aggiornati

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,27 +15,27 @@ export default function Home({ color }: { color: string }) {
     const currentTextColor = textColors[color] || textColors["emerald"]
     const currentGlow = glowColors[color] || glowColors["emerald"]
     return (
-        <div className="relative w-full text-left pt-30 px-10">
+        <div className="relative w-full text-left pt-15 md:pt-30 px-10">
       
         <div className={`absolute top-0 left-1 md:left-1/2 -translate-x-1/2 w-96 h-96 ${currentGlow} rounded-full blur-[100px] pointer-events-none`} />
 
             <div className="relative flex items-center justify-center flex-col">
-                <h1 className="text-5xl md:text-8xl font-black text-white">
+                    <h1 className="text-5xl md:text-8xl font-black text-white">
                 SELECT <span className={`${currentTextColor}`}>*</span> FROM <span className={`${currentTextColor} lowercase`}>noyla</span>
                 </h1>
                 <p className="text-slate-400 mt-2 text-xl">Query OK, 1 <span className={`${currentTextColor} font-semibold text-2xl`}>
                 <TypeAnimation
                     sequence={[
-                    'JUNIOR FULL STACK DEVELOPER',
+                    'FULL STACK EXPLORER',
                     1500,
-                    'TECH ENTHUSIAST',
+                    'DATA & WEB ENTHUSIAST',
                     1500,
-                    'BREAKER & FIXER OF THINGS',
+                    'CURIOUS DEVELOPER',
                     1500,
                     ]}
                     repeat={Infinity}
                 />    
-                </span> affected (0.02 sec)</p></div>
+                </span>affected (0.02 sec)</p></div>
             </div>
             
         )
